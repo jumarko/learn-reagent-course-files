ac5d568c0b3b44ea18aa348086e43a21b4ca210c
Juraj Martinka <jumarko@gmail.com>
2022-02-15 08:26:25 +0100

    02 App setup: finished

--- giggin/src/giggin/core.cljs
+++ giggin/src/giggin/core.cljs
@@ -1,12 +1,16 @@
 (ns giggin.core
   (:require [reagent.core :as r]))
 
+;; our first component
+;; this is an equivalent to function app() { return (<div class="container"></div>)}
 (defn app
   []
+  ;; '.' means class, you can also use '#' for ID
   [:div.container])
 
 (defn ^:export main
   []
+  ;; this returns a function
   (r/render
     [app]
     (.getElementById js/document "app")))
