7de107b8cea33d8577baf89d95f2a9e7fc4a9b45
Juraj Martinka <jumarko@gmail.com>
2022-02-21 15:15:26 +0100

    Displaying gigs.

--- giggin/src/giggin/components/gigs.cljs
+++ giggin/src/giggin/components/gigs.cljs
@@ -1,4 +1,16 @@
-(ns giggin.components.gigs)
+(ns giggin.components.gigs
+  (:require [giggin.state :as state]))
 
 (defn gigs []
-  [:h2 "gigs"])
+  [:main
+   [:div.gigs (map
+               (fn [{:keys [id img title price desc]}]
+                 [:div.gig {:key id} ;; notice adding unique key: https://reactjs.org/docs/lists-and-keys.html#keys
+                  [:img.gig__artwork {:src img :alt title}]
+                       [:div.gig__body
+                        [:div.gig__title
+                         [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "add to order"}
+                          [:i.icon.icon--plus]] title]
+                        [:p.gig__price price]
+                        [:p.gig__desc desc]]])
+               (vals @state/gigs))]])
