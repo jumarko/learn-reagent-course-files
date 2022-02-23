b63f97084daebe43ff9daf5f14eac65d39187cf5
Juraj Martinka <jumarko@gmail.com>
2022-02-23 11:25:01 +0100

    10 Order total

--- giggin/src/giggin/components/orders.cljs
+++ giggin/src/giggin/components/orders.cljs
@@ -1,6 +1,11 @@
 (ns giggin.components.orders
   (:require [giggin.state :as state]))
 
+(defn total [orders gigs]
+  (reduce-kv (fn [sum id quantity] (+ sum (* (get-in gigs [id :price]) quantity)))
+             0
+             orders))
+
 (defn orders []
   [:aside
    [:div.order
@@ -15,4 +20,20 @@
          ;; \u00D7 is '@'
          [:p.title (str title " \u00D7 " quant)]]
         [:div.action
-         [:div.price (* price quant)]]])]]])
+         [:div.price (* price quant)]
+         [:button.btn.btn--link.tooltip
+          {:data-tooltip "Remove"
+           :on-click #(swap! state/orders dissoc id)}
+          [:i.icon.icon--cross]]]])]
+    [:div.total
+     [:hr]
+     [:div.titem
+      [:div.content "Total"]
+      [:div.action
+       [:div.price (total @state/orders @state/gigs)]]
+      [:button.btn.btn--link.tooltip
+       {:data-tooltip "Remove all"
+        :on-click #(reset! state/orders {})}
+       [:i.icon.icon--delete]]
+      ]
+     ]]])
